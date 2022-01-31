96ffaa24155b319f23be818cec27563240a8c02d
MounikaAlloju <mounika.alloju@gmail.com>
2022-01-31 13:07:10 +0530

    branch 1 added

--- src/com/bridgelabz/day2/DisplayUnits.java
+++ src/com/bridgelabz/day2/DisplayUnits.java
@@ -5,19 +5,38 @@ import java.util.Scanner;
 public class DisplayUnits {
 
 	public static void main(String args[]) {
-		
-	int num;
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a Number");
-	num=sc.nextInt();
-	
-	if(num==1) {
-		
-		
-		
+
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		n = sc.nextInt();
+		int one=1,tens=1,hundred=1,thousand=1;
+		 one=(n/1)%10;
+		tens= (n/10)%10;
+		hundred = (n/100)%10;
+
+		if(one==1) {
+			System.out.println("One");
+		}
+		else if(tens==1) {
+			System.out.println("tens");
+		}
+		else if(hundred==1) {
+			System.out.println("hundred");
+		}
+		else if(thousand==1) {
+			System.out.println("thousand");
+		}
+		else {
+			System.out.println("invalid");
+		}
+			
+
+
+
+
+
+
+
 	}
-	
-	
-	
-	}
-	}
+}
